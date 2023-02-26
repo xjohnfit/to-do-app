5b05236e3c1e150ca749ef82cc1bf699772aead7
John Rocha <xjohnfitcodes@gmail.com>
2023-02-26 14:05:37 -0500

    Modal done

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,60 @@
-const Modal = () => {
+import { useState } from 'react';
+
+
+const Modal = ({ mode, setShowModal}) => {
+
+    const editMode = mode === 'edit' ? true : false;
+
+    const [data, setData] = useState({
+      user_email: "",
+      title: "",
+      progress: "",
+      date: editMode ? "" : new Date()
+    });
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+
+      setData(data => ({
+        ...data,
+        [name] : value
+      }));
+
+      console.log(data);
+    }
+
     return (
-      <div>
+      <div className="overlay">
+        <div className="modal">
+          <div className="form-title-container">
+            <h3>Lets {mode} your task</h3>
+            <button onClick={() => setShowModal(false)}>X</button>
+          </div>
+
+          <form action="">
+            <input 
+              required
+              maxLength={30}
+              placeholder="Your task"
+              name="title"
+              value={data.title}
+              onChange={handleChange}
+            />
+            <br />
+            <label htmlFor="range">Drag to select your progress</label>
+            <input 
+              required
+              type="range" 
+              id="range"
+              min="0" 
+              max="100" 
+              name="progress" 
+              value={data.progress}
+              onChange={handleChange}
+            />
+            <input className={mode} type="submit" />
+          </form>
+        </div>
       </div>
     );
   }
